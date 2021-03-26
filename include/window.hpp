0872b3e61442cc13c394516d8227a93ae6d4894a
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-03-26 14:36:33 -0300

    Added "win" object

--- include/window.hpp
+++ include/window.hpp
@@ -10,22 +10,12 @@ class window {
 private:
 	SDL_Window* win;
 	SDL_Renderer* ren;
-	const int w, h, flags;
+	const int w, h;
 
 public:
 	window(const char* title, const int w, const int h, const int flags = SDL_WINDOW_SHOWN) :
-		win(nullptr), ren(nullptr), w(w), h(h), flags(flags) {
-		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
-		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
-	}
-	window(const char* title, const window& wnd) :
-		win(nullptr), ren(nullptr), w(wnd.get_w()), h(wnd.get_h()), flags(wnd.get_flags()) {
-		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
-		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
-	}
-	window(const char* title, const window&& wnd) :
-		win(nullptr), ren(nullptr), w(wnd.get_w()), h(wnd.get_h()), flags(wnd.get_flags()) {
-		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
+		win(nullptr), ren(nullptr), w(w), h(h) {
+		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, flags);
 		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
 	}
 	~window() {
@@ -37,7 +27,6 @@ public:
 	SDL_Renderer* get_ren() const { return this->ren; }
 	int get_w() const { return this->w; }
 	int get_h() const { return this->h; }
-	int get_flags() const { return this->flags; }
 
 	void clear() const;
 	void clear(const int r, const int g, const int b) const;
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,19 @@
-int main() { }
+#include <SDL2/SDL.h>
+#include <SDL2/SDL.h>
+#include <window.hpp>
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+
+    const window win = window("Snake Game", 720, 480);
+
+    win.clear(100, 100, 0);
+    win.update();
+
+    SDL_Delay(3000);
+
+    SDL_Quit();
+    IMG_Quit();
+    return 0;
+}
