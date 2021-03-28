066302899a7d8f03e12d6bae921bb3122702fbfc
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-03-28 12:24:35 -0300

    Added "snake::tile_map" attribute

--- include/snake.hpp
+++ include/snake.hpp
@@ -6,21 +6,25 @@
 #include <SDL2/SDL.h>
 #include <vec2f.hpp>
 #include <image.hpp>
+#include <map.hpp>
 
 
 
 class snake {
 private:
 	image* spr;
+	map tile_map;
 	std::vector<vec2f> pos;
 
 public:
 	snake(SDL_Renderer* ren) :
 		spr(new image(ren, "res/sprites.png", { 0, 0, 32, 16 }, { 0, 0, 64 * 4, 64 * 2 })),
+		tile_map(map()),
 		pos( { vec2f(2, 0), vec2f(1, 0), vec2f(0, 0) } ) { }
 	~snake() { }
 
 	image* get_spr() const { return this->spr; }
-	vec2f get_pos(unsigned i) { return this->pos[i]; }
+	const map& get_tile_map() const { return this->tile_map; }
+	vec2f get_pos(unsigned i) const { return this->pos[i]; }
 
 };
